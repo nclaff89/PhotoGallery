ba158a2e37734b52779a1259a728321bd9c4b99d
Nick <nicholas.claffey@outlook.com>
2020-04-15 22:25:24 -0500

    Paging challenge solved

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -9,7 +9,6 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import androidx.paging.PagedList
 import androidx.paging.PagedListAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.GridLayoutManager
@@ -23,13 +22,13 @@ class PhotoGalleryFragment: Fragment() {
     private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
 
     private lateinit var photoRecyclerView: RecyclerView
-    //private lateinit var  swipeRefreshLayout: SwipeRefreshLayout
-
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
 
         photoGalleryViewModel = ViewModelProviders.of(this).get(PhotoGalleryViewModel::class.java)
+
+
 
 
 
@@ -51,16 +50,19 @@ class PhotoGalleryFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-//        swipeRefreshLayout.setOnRefreshListener {
-//            photoGalleryViewModel.refresh()
-//        }
+        /** chapter 24 challenge 2 **/
+        /** Initialize photo Adapter here**/
+        val mAdapter = PhotoAdapter()
         initSwipeToRefresh()
         photoGalleryViewModel.galleryItemList.observe(
             viewLifecycleOwner, Observer {
                 Log.d("thelist", "$it")
                 Log.d("thelistSize", "${it.size}")
-                    photoRecyclerView.adapter = PhotoAdapter(it)
+                /** chapter 24 challenge 2
+                 * MUST use submitList here for paging to work
+                 */
+                    mAdapter?.submitList(it)
+                photoRecyclerView.adapter = mAdapter
                 swiperefresh.isRefreshing = false
 
 
@@ -83,7 +85,8 @@ class PhotoGalleryFragment: Fragment() {
     }
 
     /** Chapter 24 challenge 2, implement PagedListAdapter **/
-    private class PhotoAdapter(private val galleryItems: PagedList<GalleryItem>)
+    /** it is important to remove the list from the constructor here **/
+    private class PhotoAdapter
         : PagedListAdapter<GalleryItem,PhotoHolder>(object : DiffUtil.ItemCallback<GalleryItem>(){
 
         override fun areItemsTheSame(oldItem: GalleryItem, newItem: GalleryItem): Boolean =
@@ -102,10 +105,14 @@ class PhotoGalleryFragment: Fragment() {
             return PhotoHolder(textView)
         }
 
-        override fun getItemCount(): Int = galleryItems.size
+        //override fun getItemCount(): Int = galleryItems.size
 
+        /** Chapter 24 challenge 2
+         * It is important to use getItem(position) here
+         */
         override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
-            val galleryItem = galleryItems[position]
+            //val galleryItem = galleryItems[position]
+            val galleryItem = getItem(position)
             galleryItem?.title?.let { holder.bindTitle(it) }
         }
 
