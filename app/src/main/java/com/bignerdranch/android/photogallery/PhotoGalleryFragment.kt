e814c6123b3504ead408932c40817980978b43ff
Nick <nicholas.claffey@outlook.com>
2020-05-01 21:30:40 -0500

    chapter 25 challenge 2

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -5,25 +5,17 @@ import android.graphics.drawable.ColorDrawable
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bignerdranch.android.photogallery.api.FlickrApi
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+
 
 private const val TAG = "PhotoGalleryFragment"
 class PhotoGalleryFragment: Fragment() {
@@ -47,7 +39,8 @@ class PhotoGalleryFragment: Fragment() {
                 val drawable = BitmapDrawable(resources, bitmap)
                 photoHolder.bindDrawable(drawable)
             }
-        lifecycle.addObserver(thumbnailDownloader.fragmentLifeCycleObserver)
+        /** chapter 25 challenge 2**/
+       //lifecycle.addObserver(thumbnailDownloader.viewLifecycleObserver)
 
     }
 
@@ -58,7 +51,7 @@ class PhotoGalleryFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         viewLifecycleOwner.lifecycle.addObserver(
-            thumbnailDownloader.viewLifecycleObserver
+            thumbnailDownloader.fragmentLifeCycleObserver
         )
         val view = inflater.inflate(R.layout.fragment_photo_gallery, container, false)
 
@@ -80,7 +73,7 @@ class PhotoGalleryFragment: Fragment() {
     override fun onDestroyView(){
         super.onDestroyView()
         viewLifecycleOwner.lifecycle.removeObserver(
-            thumbnailDownloader.viewLifecycleObserver
+            thumbnailDownloader.fragmentLifeCycleObserver
         )
     }
 
--- app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/ThumbnailDownloader.kt
@@ -6,6 +6,7 @@ import android.os.Handler
 import android.os.HandlerThread
 import android.os.Message
 import android.util.Log
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
@@ -32,18 +33,26 @@ class ThumbnailDownloader<in T>(private val responseHandler: Handler,
             @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
             fun tearDown(){
                 Log.i(TAG, "Destroying background thread")
-                quit()
-            }
-        }
-    val viewLifecycleObserver: LifecycleObserver =
-        object: LifecycleObserver{
-            @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-            fun clearQueue(){
-                Log.i(TAG, "Clearing all requests from queue")
+                /** chapter25 challenge2 */
                 requestHandler.removeMessages(MESSAGE_DOWNLOAD)
                 requestMap.clear()
+                /** */
+                quit()
+
+
             }
+
+
         }
+//    val viewLifecycleObserver: LifecycleObserver =
+//        object: LifecycleObserver{
+//            @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+//            fun clearQueue(){
+//                Log.i(TAG, "Clearing all requests from queue")
+//                requestHandler.removeMessages(MESSAGE_DOWNLOAD)
+//                requestMap.clear()
+//            }
+//        }
 
     private var hasQuit = false
     private lateinit var requestHandler: Handler
